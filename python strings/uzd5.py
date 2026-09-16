90ecc00f0377f269f723fca23542b466a8a9a07b
uxokpro1234 <81376237+uxokpro1234@users.noreply.github.com>
2026-09-16 22:05:40 +0300

    Add function to calculate volume of a cuboid

---
+++ python strings/uzd5.py
@@ -0,0 +1,8 @@
+def Tilp(a,b,c):
+  v=a*b*c
+  return v
+a = int(input("Ievadiet paralēlskaldņa ribu garumu a "))
+b= int(input("Ievadiet paralēlskaldņa ribu garumu b "))
+c= int(input("Ievadiet paralēlskaldņa ribu garumu c "))
+rez=Tilp(a,b,c)
+print("Kuba tilpums:",rez)
