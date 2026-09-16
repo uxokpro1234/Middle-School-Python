b9a7496b062585e991275d2c66fee26fb90706bd
uxokpro1234 <81376237+uxokpro1234@users.noreply.github.com>
2026-09-16 21:58:56 +0300

    Add array initialization and user input handling
    Initialize an array and print its elements based on user input.

---
+++ arrays/uzd6.py
@@ -0,0 +1,11 @@
+N = 10
+A = [0] * N
+print ( "Ievadiet X. Masīva elementi tiks sākot no X :" )
+X=int(input())
+for i in range(N):
+  A[i] 
+
+print ( "Masīvs:" )
+for i in range(N):
+  print("A[", i, "]=", sep="", end="")
+  print ( A[i], " ", sep="")  
