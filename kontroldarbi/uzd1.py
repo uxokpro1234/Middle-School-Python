19301fc33fb8951545707b562883d7425c2140b1
uxokpro1234 <81376237+uxokpro1234@users.noreply.github.com>
2026-09-16 22:01:38 +0300

    Add function to calculate product of three numbers

---
+++ kontroldarbi/uzd1.py
@@ -0,0 +1,8 @@
+a = int ( input ( "Ievadiet a vertību " ) )
+b = int ( input ( "Ievadiet b verību " ) )
+c = int ( input ( "Ievadiet c vertību " ) )
+
+def count ( a , b , c ):
+  return a * b * c 
+
+print ( "Summa:" , count ( a , b , c ) )
