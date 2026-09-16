b3e0a90e6189746901e2d9cbed3fd4424bc169af
uxokpro1234 <81376237+uxokpro1234@users.noreply.github.com>
2026-09-16 22:04:50 +0300

    Add function to calculate average of five numbers

---
+++ python strings/uzd2.py
@@ -0,0 +1,11 @@
+def Avg(a,b,c,d,e):
+  r=(a+b+c+d+e)/5
+  return r
+a = int(input("Input a "))
+b = int(input("Input b "))
+c = int(input("Input c "))
+d = int(input("Input d "))
+e = int(input("Input e "))
+rez=Avg(a,b,c,d,e)
+print("Average:",rez)
+
